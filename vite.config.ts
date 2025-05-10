46168dc4d5e5813a3b7c7d2bd7073ff02491e188
André Santiago <andre.mpc.santiago@gmail.com>
2025-05-10 16:14:13 +0200

    feat: update project configuration with Vite build support

---
+++ vite.config.ts
@@ -0,0 +1,46 @@
+import { defineConfig, Plugin, Rollup } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+import fs from "fs";
+import path from "path";
+
+const handleIndexHtml: Plugin = {
+	name: "handle-index-html",
+	writeBundle(options: Rollup.NormalizedOutputOptions): void {
+		fs.cpSync(
+			path.resolve(options.dir ?? "dist", "public", "index.html"),
+			path.resolve(options.dir ?? "dist", "index.html")
+		);
+		fs.rmSync(path.resolve(options.dir ?? "dist", "public"), {
+			recursive: true,
+			force: true
+		});
+	}
+};
+
+export default defineConfig({
+	root: "./",
+	publicDir: "public",
+	build: {
+		outDir: "dist",
+		emptyOutDir: true,
+		sourcemap: true,
+		rollupOptions: {
+			input: {
+				main: path.resolve(__dirname, "public", "index.html")
+			}
+		}
+	},
+	resolve: {
+		alias: {
+			src: "/src"
+		}
+	},
+	server: {
+		host: true,
+		open: true
+	},
+	plugins: [
+		tsconfigPaths(),
+		handleIndexHtml
+	]
+});
